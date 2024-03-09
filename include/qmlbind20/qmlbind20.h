88e8da283f51e1a94687a78c7adeaf73a16a44b3
whs31 <75864292+whs31@users.noreply.github.com>
2024-03-09 18:39:07 +0300

    module definition

--- include/qmlbind20/qmlbind20.h
+++ include/qmlbind20/qmlbind20.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#include <string>
+#include <leaf/global.h>
+#include <qqml.h>
+
+namespace qmlbind20
+{
+  using std::string;
+  using std::string_view;
+  using namespace leaf::types;
+
+  class module_
+  {
+    public:
+      module_(string_view name, int version_major, int version_minor);
+
+      [[nodiscard]] auto name() const -> string_view;
+      [[nodiscard]] auto version() const -> string;
+      [[nodiscard]] auto version_major() const -> int;
+      [[nodiscard]] auto version_minor() const -> int;
+
+    protected:
+      string m_name;
+      int m_version_major;
+      int m_version_minor;
+  };
+}
